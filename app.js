117318c5cf257d44abb68dd43ae32399207704ee
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-10-19 22:14:18 +0900

    .gitignore add

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const router = require('./routes/commentRoute');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use('/', router);
 
 module.exports = app;
